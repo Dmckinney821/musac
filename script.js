348d89af286ef12c92aac8b2ca2d5c3b60c73852
Dan McKinney <3dhdaniel@gmail.com>
2020-03-21 11:25:30 -0400

    weekend

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const musicContainer = document.getElementById('music_container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progreeContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+const songs = ['hey', 'summer', 'ukulele'];
