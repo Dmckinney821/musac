21498519482b377cf756bab8b97bf21446dc4e62
Dan McKinney <3dhdaniel@gmail.com>
2020-03-21 11:49:00 -0400

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const musicContainer = document.getElementById('music_container');
+const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
@@ -9,4 +9,86 @@ const progreeContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
+// song titles
 const songs = ['hey', 'summer', 'ukulele'];
+
+let songIndex = 1;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song){
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
+};
+
+function playSong() {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fa').classList.remove('fa-play')
+    playBtn.querySelector('i.fa').classList.remove('fa-pause')
+
+    audio.play()
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fa').classList.add('fa-play')
+    playBtn.querySelector('i.fa').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong() {
+    songIndex++;
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration
+}
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+    
+})
+
+// change the song
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+// time and song update
+audio.addEventListener('timeupdate', updateProgress)
+
+progreeContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
